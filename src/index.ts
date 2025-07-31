3117dd5e2d496a8afc8045f1c09b28859ac56364
Aayush Garg <masteraayushgarg@gmail.com>
2025-07-31 12:45:59 +0530

    Added bullboard for monitoring

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 import emailRouter from './routes/email.route'
+import bullBoardAdapter from './bullboard'
+
 const app = express();
 
 app.use(express.json());
 app.use('/',emailRouter);
-
+app.use('/admin/queues',bullBoardAdapter.getRouter());
 
 const PORT = process.env.PORT || 4000;
 
